b0a56b352c5d24cd1b9c65d1188aa5d92731ad78
Janyl <janyla_i@mail.ru>
2023-06-23 19:02:10 +0600

    added Class monkey

---
+++ src/test/java/codingbat/Janyla/monkeyTrouble.java
@@ -0,0 +1,13 @@
+package codingbat.Janyla;
+
+public class monkeyTrouble {
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if (aSmile && bSmile) {
+            return true;
+        }
+        if (!aSmile && !bSmile) {
+            return true;
+        }
+        return false;
+    }
+}
