5fd5d04d8d330e23f89c22d0d066dc6ba4b79d0d
Dastan Baidolotov <dastan2134@gmail.com>
2023-06-23 17:04:02 +0600

    addedThreeMethod

---
+++ src/test/java/codingbat/dastan/SleepIn.java
@@ -0,0 +1,13 @@
+package codingbat.dastan;
+
+public class SleepIn {
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if (!weekday || vacation) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+}
