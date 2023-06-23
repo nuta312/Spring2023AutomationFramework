710d41d2a69a9f0e1e55b43271deeb9517ea8bda
Tulenbaeva Altynai <tulenbaevaltyn>
2023-06-23 15:30:54 +0600

    removed

--- src/test/java/codingbat/altynai/WurmUp1.java
+++ src/test/java/codingbat/altynai/WurmUp1.java
@@ -2,6 +2,6 @@ package codingbat.altynai;
 
 public class WurmUp1 {
     public static void main(String[] args) {
-        System.out.println("jsubf");
+        System.out.println("jsub");
     }
 }
