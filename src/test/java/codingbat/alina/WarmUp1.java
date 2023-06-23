ff5b9f2c65483c9501b0b00442c51c19022e12e6
Alina Raikanova <raikanovaalina2@gmail.com>
2023-06-23 23:34:57 +0600

    monkeyTrouble

--- src/test/java/codingbat/alina/WarmUp1.java
+++ src/test/java/codingbat/alina/WarmUp1.java
@@ -6,4 +6,14 @@ public class WarmUp1 {
         return !weekday || vacation;
 
     }
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if (aSmile && bSmile){
+            return true;
+        }
+        if(!aSmile && ! bSmile){
+            return true;
+        }
+        return false;
+    }
+
 }
