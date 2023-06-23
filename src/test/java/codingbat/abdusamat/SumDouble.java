b83f114ebcae90327f04ecdbc14cc895fc0d83cb
Abdusamat <abdusamattoktoganov@gmail.com>
2023-06-23 17:57:48 +0600

    diffmethod

--- src/test/java/codingbat/abdusamat/SumDouble.java
+++ src/test/java/codingbat/abdusamat/SumDouble.java
@@ -2,10 +2,7 @@ package codingbat.abdusamat;
 
 public class SumDouble {
     public int sumDouble(int a, int b) {
-        // Store the sum in a local variable
         int sum = a + b;
-
-        // Double it if a and b are the same
         if (a == b) {
             sum = sum * 2;
         }
