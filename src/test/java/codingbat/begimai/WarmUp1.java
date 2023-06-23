60cac944172b95cba7d74594d62b49ea8fb223ba
BegimaiAibekova <begimai.aibekova@gmail.com>
2023-06-23 15:25:46 +0600

    addedSleepInClass

--- src/test/java/codingbat/begimai/WarmUp1.java
+++ src/test/java/codingbat/begimai/WarmUp1.java
@@ -2,6 +2,14 @@ package codingbat.begimai;
 
 public class WarmUp1 {
     public static void main(String[] args) {
+        boolean weekday = true;
+        boolean vacation = false;
+            if(!weekday || vacation==true){
+                System.out.println(true);
+            }else{
+                System.out.println(false);
+        }
+
 
     }
 }
