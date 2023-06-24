0343f927a5be4fda5452d6faf13ca353d3846a85
Tulenbaeva Altynai <tulenbaevaltyn>
2023-06-24 23:05:44 +0600

    added method notString

--- src/test/java/codingbat/altynai/WarmUp1.java
+++ src/test/java/codingbat/altynai/WarmUp1.java
@@ -2,14 +2,26 @@ package codingbat.altynai;
 
 public class WarmUp1 {
     public boolean nearHundred(int n) {
-        return((Math.abs(100 - n) <= 10) ||
+        return ((Math.abs(100 - n) <= 10) ||
                 (Math.abs(200 - n) <= 10));
     }
+
     public boolean posNeg(int a, int b, boolean negative) {
-        if (negative){
-            return (a<0&&b<0);}
-        else{return ((a<0&&b>0)||(a>0&&b<0));
+        if (negative) {
+            return (a < 0 && b < 0);
+        } else {
+            return ((a < 0 && b > 0) || (a > 0 && b < 0));
+        }
+    }
+
+    public String addNotToString(String str) {
+        if (str.startsWith("not ")) {
+            return str;
+        } else {
+            return "not " + str;
         }
     }
 
 }
+
+
