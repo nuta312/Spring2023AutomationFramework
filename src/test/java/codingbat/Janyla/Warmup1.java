7efe7e24d101b4d11d0e90b68c876ae0f1762ea1
Janyl <janyla_i@mail.ru>
2023-06-24 23:43:10 +0600

    added method

--- src/test/java/codingbat/Janyla/Warmup1.java
+++ src/test/java/codingbat/Janyla/Warmup1.java
@@ -5,7 +5,47 @@ public class Warmup1 {
         if (!weekday || vacation){
             return true;
         }
-
         return false;
+    }
+
+    public boolean makes10(int a, int b) {
+        boolean s= true;
+        boolean d= false;
+        if(a==10|| b==10){
+            return s;
+        }if ((a+b)==10){
+            return s;
+        }return d;
+
+    }
+
+
+    public boolean nearHundred(int n) {
+        return ((Math.abs(100 - n) <= 10) ||
+                (Math.abs(200 - n) <= 10));
+    }
+
+    public boolean posNeg(int a, int b, boolean negative) {
+        if (negative) {
+            return (a < 0 && b < 0);
+        }
+        else {
+            return ((a < 0 && b > 0) || (a > 0 && b < 0));
+        }
+    }
+    public String notString(String str) {
+        String b = " ";
+        if (!str.contains("not")) {
+            b ="not "+ str ;
+            return b;
+        }
+        if (str.startsWith("not")) {
+            b = str;
+            return b;
+        }if (str.contains("not")){
+            b= "not "+ str ;
+            return b;
+        }return b;
+
     }
 }
