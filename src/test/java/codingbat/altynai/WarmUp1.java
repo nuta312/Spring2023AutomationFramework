e826bf9aa08c4de190c2cf43ad0db15057405b3f
Tulenbaeva Altynai <tulenbaevaltyn>
2023-06-24 22:46:51 +0600

    added method posNeg

--- src/test/java/codingbat/altynai/WarmUp1.java
+++ src/test/java/codingbat/altynai/WarmUp1.java
@@ -5,5 +5,11 @@ public class WarmUp1 {
         return((Math.abs(100 - n) <= 10) ||
                 (Math.abs(200 - n) <= 10));
     }
+    public boolean posNeg(int a, int b, boolean negative) {
+        if (negative){
+            return (a<0&&b<0);}
+        else{return ((a<0&&b>0)||(a>0&&b<0));
+        }
+    }
 
 }
