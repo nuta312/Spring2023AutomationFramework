d9da554c8f935ea1e3856372b5076a33c148bcfd
kulpunai12 <kulpunai_B@mail.ru>
2023-06-24 23:08:26 +0300

    feature/10tasks

--- src/test/java/codingbat/kulpunai/warmup1.java
+++ src/test/java/codingbat/kulpunai/warmup1.java
@@ -29,5 +29,48 @@ public class warmup1 {
         return (talking && (hour >= 1 && hour < 7 || hour <= 23 && hour > 20));
 
     }
+    //6
+    public boolean makes10(int a, int b) {
+
+        if(a == 10 || b ==10 || (a + b == 10)){
+            return true;
+        }else {
+            return false;
+        }
+    }
+    //7
+    public boolean nearHundred(int n) {
+        if((n >= 90 && n <= 100) || (n >= 100 && n <= 110 ) ||( n >= 190 && n <= 200) ||(n >= 200 && n <= 210)){
+            return true;
+        } else{
+            return false;
+        }
+    }
+    //8
+    public boolean posNeg(int a, int b, boolean negative) {
+        if ((((a > 0) && (b < 0)) && (negative == false)) || (((a < 0) && (b > 0) && (negative == false))) ||
+                ((a < 0) && (b < 0)) && (negative == true)) {
+            return true;
+        } else {
+            return false;
+
+        }
+    }
+    //9
+    public String notString(String str) {
+        if (str.length() >= 3 && str.substring(0, 3).equals("not")) {
+            return str;
+        }else{
+            return "not " + str;
+
+        }
+    }
+    //10
+    public String missingChar(String str, int n) {
+
+        StringBuilder stra = new StringBuilder();
+        str.equals(stra.deleteCharAt(n).toString());
+        return str;
+    }
 
 }
