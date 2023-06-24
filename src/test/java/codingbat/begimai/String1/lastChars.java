b18274a3b9ce8ede4bc499a2a9d515b6e7f4571b
BegimaiAibekova <begimai.aibekova@gmail.com>
2023-06-24 10:26:42 +0600

    addedFiveStringTasks

---
+++ src/test/java/codingbat/begimai/String1/lastChars.java
@@ -0,0 +1,19 @@
+package codingbat.begimai.String1;
+
+public class lastChars {
+    public String lastChars(String a, String b) {
+
+        if(a.length() == 0)
+
+            a = "@";
+
+        if(b.length() == 0)
+
+            b = "@";
+
+
+
+        return a.substring(0,1) + b.substring(b.length()-1, b.length());
+
+    }
+}
