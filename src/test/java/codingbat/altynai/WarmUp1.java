64ab7ba6002bd03cd8b34b7c8ef497c63828f7c4
Tulenbaeva Altynai <tulenbaevaltyn>
2023-06-25 19:50:00 +0600

    added method max1020

--- src/test/java/codingbat/altynai/WarmUp1.java
+++ src/test/java/codingbat/altynai/WarmUp1.java
@@ -138,7 +138,17 @@ public class WarmUp1 {
             return false;
         }
     }
+    public int max1020(int a, int b) {
+        if (b>a){
+            int temp=a;
+            a=b;
+            b=temp;
+        }
 
+        if (a>=10 && a<=20) return a;
+        if (b>=10 && b<=20) return b;
+        return 0;
+    }
 }
 
 
