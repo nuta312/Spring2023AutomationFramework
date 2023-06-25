87750bbf9cd6f62a9cac9a9e2637de727af6ce01
Tulenbaeva Altynai <tulenbaevaltyn>
2023-06-25 19:47:39 +0600

    added method close10

--- src/test/java/codingbat/altynai/WarmUp1.java
+++ src/test/java/codingbat/altynai/WarmUp1.java
@@ -117,6 +117,18 @@ public class WarmUp1 {
             max=c;
         } return max;
     }
+    public int close10(int a, int b) {
+        int diffA = Math.abs(a - 10);
+        int diffB = Math.abs(b - 10);
+
+        if (diffA < diffB) {
+            return a;
+        } else if (diffB < diffA) {
+            return b;
+        } else {
+            return 0;
+        }
+    }
 }
 
 
