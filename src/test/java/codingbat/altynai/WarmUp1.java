edd31efa735ad007b5609d73ae8f7d7356e2e944
Tulenbaeva Altynai <tulenbaevaltyn>
2023-06-25 19:39:59 +0600

    added method in1020

--- src/test/java/codingbat/altynai/WarmUp1.java
+++ src/test/java/codingbat/altynai/WarmUp1.java
@@ -71,7 +71,13 @@ public class WarmUp1 {
             return false;
         }
     }
-
+    public boolean in1020(int a, int b) {
+        if ((a >= 10 && a <= 20) || (b >= 10 && b <= 20)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
 
