414cd0a6a2fa519d53e13aa0ba397db55c9bdb5f
Tulenbaeva Altynai <tulenbaevaltyn>
2023-06-25 19:48:41 +0600

    added method in3050

--- src/test/java/codingbat/altynai/WarmUp1.java
+++ src/test/java/codingbat/altynai/WarmUp1.java
@@ -129,6 +129,16 @@ public class WarmUp1 {
             return 0;
         }
     }
+    public boolean in3050(int a, int b) {
+        if (a>=30 && a<=40 && b>=30 && b<=40) {
+            return true;
+        } if (a>=40 && a<=50 && b>=40 && b<=50) {
+            return true;
+        } else{
+            return false;
+        }
+    }
+
 }
 
 
