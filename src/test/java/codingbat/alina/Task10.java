df2477818fe2ce0d411e4288708b7bde6c57e8cd
Alina Raikanova <raikanovaalina2@gmail.com>
2023-06-26 17:29:50 +0600

    max1020

--- src/test/java/codingbat/alina/Task10.java
+++ src/test/java/codingbat/alina/Task10.java
@@ -74,4 +74,18 @@ public class Task10 {
             return false;
         }
     }
+    public int max1020(int a, int b) {
+        if (b > a) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        if (a >= 10 && a <= 20){
+            return a;
+        }
+        if (b >= 10 && b <= 20){
+            return b;
+        }
+        return 0;
+    }
 }
