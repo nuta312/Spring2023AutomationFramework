1f52df904411990f6d719a701c701d5352fe3de1
Alina Raikanova <raikanovaalina2@gmail.com>
2023-06-26 22:11:20 +0600

    test

--- src/test/java/codingbat/Nariman/SecondcodigHW.java
+++ src/test/java/codingbat/Nariman/SecondcodigHW.java
@@ -21,10 +21,11 @@ public class SecondcodigHW {
     public boolean posNeg(int a, int b, boolean negative) {
         if (negative) {
             return (a < 0 && b < 0);
+
         } else {
             return ((a < 0 && b > 0) || (a > 0 && b < 0));
         }
-
+    }
         public String notString (String str){
             if (str.length() >= 3 && str.substring(0, 3).equals("not")) {
                 return str;
@@ -41,4 +42,4 @@ public class SecondcodigHW {
 
     }
 
-}
+
