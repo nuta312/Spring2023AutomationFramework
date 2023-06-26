feb47973fad6b36bb9b6b3a43bc861dd833f4bd6
Alina Raikanova <raikanovaalina2@gmail.com>
2023-06-26 17:30:40 +0600

    stringE

--- src/test/java/codingbat/alina/Task10.java
+++ src/test/java/codingbat/alina/Task10.java
@@ -88,4 +88,13 @@ public class Task10 {
         }
         return 0;
     }
+    public boolean stringE(String str) {
+        int count=0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='e'){
+                count++;
+            }
+        }
+        return (count>=1&&count<=3);
+    }
 }
