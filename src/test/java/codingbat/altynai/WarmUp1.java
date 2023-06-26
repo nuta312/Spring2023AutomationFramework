1c3d28e8fc203ce6898da11ae9cd4a889d83dc52
Tulenbaeva Altynai <tulenbaevaltyn>
2023-06-26 16:30:56 +0600

    added method everyNth

--- src/test/java/codingbat/altynai/WarmUp1.java
+++ src/test/java/codingbat/altynai/WarmUp1.java
@@ -169,6 +169,13 @@ public class WarmUp1 {
             return str.substring(0, str.length() - 3) + lastThreeChars.toUpperCase();
         }
     }
+    public String everyNth(String str, int n) {
+        String result="";
+        for (int i=0; i<str.length(); i=i+n) {
+            result =result+str. charAt(i);
+
+        } return result;
+    }
 
 }
 
