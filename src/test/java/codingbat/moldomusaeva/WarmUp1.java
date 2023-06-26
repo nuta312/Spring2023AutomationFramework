ef44fe1fd9cbf8987d49f500ca78dca8bf6bf414
Aigerim Moldomusaeva <moldomusaevaaigerim@gmail.com>
2023-06-26 13:19:42 +0600

    added 15 WarpmUp1 solutions

--- src/test/java/codingbat/moldomusaeva/WarmUp1.java
+++ src/test/java/codingbat/moldomusaeva/WarmUp1.java
@@ -1,4 +1,168 @@
 package codingbat.moldomusaeva;
 
+
 public class WarmUp1 {
+
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if (!weekday || vacation) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if (!aSmile == bSmile) {
+            return false;
+        }
+        return true;
+
+    }
+
+    public int sumDouble(int a, int b) {
+        int sum = a + b;
+        if (a == b) {
+            sum = sum * 2;
+        }
+        return sum;
+    }
+
+    public int diff21(int n) {
+        if (n <= 21) {
+            return 21 - n;
+        } else {
+            return (n - 21) * 2;
+
+        }
+    }
+
+    public boolean parrotTrouble(boolean talking, int hour) {
+        return (talking && (hour < 7 || hour > 20));
+    }
+
+    public boolean makes10(int a, int b) {
+        return (a == 10 || a + b == 10 || b == 10);
+    }
+
+    public boolean posNeg(int a, int b, boolean negative) {
+        if (negative) {
+            return (a < 0 && b < 0);
+        } else {
+            return ((a < 0 && b > 0) || (a > 0 && b < 0));
+        }
+
+
+    }
+
+    public String notString(String str) {
+        if (str.length() >= 3 && str.substring(0, 3).equals("not")) {
+            return str;
+        }
+        return "not " + str;
+    }
+
+    public boolean nearHundred(int n) {
+        return ((Math.abs(100 - n) <= 10) || ((Math.abs(200 - n) <= 10)));
+    }
+
+    public String missingChar(String str, int n) {
+        String front = str.substring(0, n);
+        String back = str.substring(n + 1, str.length());
+        return front + back;
+    }
+
+    public String frontBack(String str) {
+        if (str.length() <= 1) return str;
+        String mid = str.substring(1, str.length() - 1);
+        return str.charAt(str.length() - 1) + mid + str.charAt(0);
+
+
+    }
+
+    public String front3(String str) {
+        if (str.length() > 3) {
+            return str.substring(0, 3) + str.substring(0, 3) + str.substring(0, 3);
+        } else {
+            return str + str + str;
+        }
+    }
+
+    public String backAround(String str) {
+        String last = str.substring(str.length() - 1);
+        return last + str + last;
+    }
+
+    public boolean or35(int n) {
+        return (n % 3 == 0 || n % 5 == 0);
+    }
+
+    public String front22(String str) {
+        int take = 2;
+        if (take > str.length()) {
+            take = str.length();
+        }
+        String front = str.substring(0, take);
+        return front + str + front;
+
+    }
+
+    public boolean startHi(String str) {
+
+        if (str.length() < 2) return false;
+        String firstTwo = str.substring(0, 2);
+        if (firstTwo.equals("hi")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean icyHot(int temp1, int temp2) {
+        if (temp1 < 0 && temp2 > 100 || temp2 < 0 && temp1 > 100) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean in1020(int a, int b) {
+        return ((a >= 10 && a <= 20) || (b >= 10 && b <= 20));
+    }
+
+    public boolean hasTeen(int a, int b, int c) {
+        return (a >= 13 && a <= 19) ||
+                (b >= 13 && b <= 19) ||
+                (c >= 13 && c <= 19);
+    }
+
+    public boolean loneTeen(int a, int b) {
+        boolean aTeen = (a >= 13 && a <= 19);
+        boolean bTeen = (b >= 13 && b <= 19);
+
+        return (aTeen && !bTeen) || (!aTeen && bTeen);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
