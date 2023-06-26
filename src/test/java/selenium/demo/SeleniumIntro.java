9ee48ad9d745cb225a99ebb907743c05f1ff75e3
Tulenbaeva Altynai <tulenbaevaltyn>
2023-06-26 20:08:42 +0600

    created new class SeleniumIntro

---
+++ src/test/java/selenium/demo/SeleniumIntro.java
@@ -0,0 +1,13 @@
+package selenium.demo;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumIntro {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://nambafood.kg/");
+    }
+}
