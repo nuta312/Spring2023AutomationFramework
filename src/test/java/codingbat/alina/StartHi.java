0100b625698b2eec50c03ec94b3d83ee960d3324
Alina Raikanova <raikanovaalina2@gmail.com>
2023-06-26 13:53:47 +0600

    startHI

--- src/test/java/codingbat/alina/StartHi.java
+++ src/test/java/codingbat/alina/StartHi.java
@@ -1,2 +1,12 @@
-package codingbat.alina;public class StartHi {
+package codingbat.alina;
+
+public class StartHi {
+    public boolean startHi(String str) {
+        if(str.startsWith("hi")){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
