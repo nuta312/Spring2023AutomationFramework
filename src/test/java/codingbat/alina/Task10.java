b66b617267de2077a6269d5c02268a5c5a9a79d5
Alina Raikanova <raikanovaalina2@gmail.com>
2023-06-26 17:24:43 +0600

    in3050

--- src/test/java/codingbat/alina/Task10.java
+++ src/test/java/codingbat/alina/Task10.java
@@ -63,4 +63,15 @@ public class Task10 {
         return 0;
 
     }
+    public boolean in3050(int a, int b) {
+        if(a>=30&&a<=40&&b>=30&&b<=40){
+            return true;
+        }
+        else if(a>=40&&a<=50&&b>=40&&b<=50){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
