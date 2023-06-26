32d067dc12e29782ccf556775664d6ba528d31b4
Alina Raikanova <raikanovaalina2@gmail.com>
2023-06-26 17:18:02 +0600

    intMax

--- src/test/java/codingbat/alina/Task10.java
+++ src/test/java/codingbat/alina/Task10.java
@@ -40,4 +40,15 @@ public class Task10 {
             return "";
         }
     }
+    public int intMax(int a, int b, int c) {
+
+        int max = a;
+        if (b > max) {
+            max = b;
+        }
+        if (c > max) {
+            max = c;
+        }
+        return max;
+    }
 }
