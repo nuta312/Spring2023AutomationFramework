f5e668c75b3cc939e2fe22d25bac44b982774f5b
Alina Raikanova <raikanovaalina2@gmail.com>
2023-06-26 17:15:39 +0600

    delDel

--- src/test/java/codingbat/alina/Task10.java
+++ src/test/java/codingbat/alina/Task10.java
@@ -1,2 +1,18 @@
-package codingbat.alina;public class Task10 {
+package codingbat.alina;
+
+public class Task10 {
+    public String delDel(String str) {
+        if(str.length()==0){
+            return "";
+        }
+        else  if(str.substring(1).startsWith("del")) {
+            StringBuilder sb = new StringBuilder(str);
+            sb.delete(1, 4);
+            return sb.toString();
+        }
+        else {
+            return str;
+        }
+    }
+
 }
