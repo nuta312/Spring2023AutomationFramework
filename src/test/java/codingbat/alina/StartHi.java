b461ff57106cb5b9cfa44f587c097377755b405f
Alina Raikanova <raikanovaalina2@gmail.com>
2023-06-26 13:56:32 +0600

    hasTeen

--- src/test/java/codingbat/alina/StartHi.java
+++ src/test/java/codingbat/alina/StartHi.java
@@ -25,4 +25,14 @@ public class StartHi {
             return false;
         }
     }
+
+    public boolean hasTeen(int a, int b, int c) {
+        if(a>=13&&a<=19||b>=13&&b<=19||c>=13&&c<=19){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
 }
