6aa24753f314d63417451ee0995006c3f29bc464
Alina Raikanova <raikanovaalina2@gmail.com>
2023-06-26 13:57:23 +0600

    loneTeen

--- src/test/java/codingbat/alina/StartHi.java
+++ src/test/java/codingbat/alina/StartHi.java
@@ -34,5 +34,13 @@ public class StartHi {
             return false;
         }
     }
-
+    public boolean loneTeen(int a, int b) {
+        if ((a >= 13 && a <= 19) && (b < 13 || b > 19)) {
+            return true;
+        } else if ((b >= 13 && b <= 19) && (a < 13 || a > 19)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
