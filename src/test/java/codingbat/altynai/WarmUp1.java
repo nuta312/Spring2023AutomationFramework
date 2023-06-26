7c68b18779c2c24408bb3dcbdb3ad8c54c525f60
Tulenbaeva Altynai <tulenbaevaltyn>
2023-06-26 16:29:05 +0600

    added method lastDigit

--- src/test/java/codingbat/altynai/WarmUp1.java
+++ src/test/java/codingbat/altynai/WarmUp1.java
@@ -158,6 +158,10 @@ public class WarmUp1 {
         }
         return (count >=1 && count <=3);
     }
+    public boolean lastDigit(int a, int b) {
+        return(a % 10 == b % 10);
+    }
+
 
 }
 
