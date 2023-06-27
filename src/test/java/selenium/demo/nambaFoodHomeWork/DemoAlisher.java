3bff7f666962d82e721afb7c03d26f0bd3e7b518
Alisher R <alisherust@gmail.com>
2023-06-27 18:08:47 +0600

    NambaFoodTestCompliete

--- src/test/java/selenium/demo/nambaFoodHomeWork/DemoAlisher.java
+++ src/test/java/selenium/demo/nambaFoodHomeWork/DemoAlisher.java
@@ -30,7 +30,34 @@ public class DemoAlisher {
         WebElement typeGiraffeCoffee = driver.findElement(By.cssSelector("a[href='/giraffe']"));
         typeGiraffeCoffee.click();
 
-        //WebElement chooseWhatToEat = driver.findElement(By.cssSelector("a[data-item-name='Бургер с томленой бараниной']"));
+        WebElement chooseWhatToEat = driver.findElement(By.xpath("//div[@data-item-name='Котлета по-киевски']//button[normalize-space()='Заказать']"));
+        chooseWhatToEat.click();
+
+        WebElement chooseBasket = driver.findElement(By.xpath("//a[@class='basket tk-basket']"));
+        chooseBasket.click();
+
+        WebElement order = driver.findElement(By.xpath(" //div[@class='order--actions']/a"));
+        order.click();
+
+        WebElement clientName = driver.findElement(By.id("food_order_client_name"));
+        clientName.sendKeys("Ali Rus");
+
+        WebElement clientAddress = driver.findElement(By.id("food_order_address"));
+        clientAddress.sendKeys("Chukotka, Noname ave.");
+
+        WebElement moneyChange = driver.findElement(By.id("food_order_money_change"));
+        moneyChange.sendKeys("1000");
+
+        WebElement submitBtn = driver.findElement(By.id("payment_cash"));
+        WebElementActions.scrollToElement(driver, submitBtn);
+        submitBtn.click();
+
+
+
+
+
+
+
 
 
 
