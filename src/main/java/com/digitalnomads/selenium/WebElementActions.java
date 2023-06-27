c5e69d8df3086c5f5351099711cf61b9dd9c4d1b
Abdusamat <abdusamattoktoganov@gmail.com>
2023-06-27 16:29:18 +0600

    dix/deletedidea

--- src/main/java/com/digitalnomads/selenium/WebElementActions.java
+++ src/main/java/com/digitalnomads/selenium/WebElementActions.java
@@ -1,2 +1,12 @@
-package com.digitalnomads.selenium;public class WebElementActions {
+package com.digitalnomads.selenium;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class WebElementActions {
+    public static void scrollToElement(WebDriver driver, WebElement element) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView(true);", element);
+    }
 }
