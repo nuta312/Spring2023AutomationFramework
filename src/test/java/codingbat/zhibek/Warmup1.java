ce9ba951b7ac4bc53d484b4ce225506da27f8018
Zhibek kadyrova <jibek.k.2202@gmail.com>
2023-06-27 17:35:53 +0600

    WarmUp1

--- src/test/java/codingbat/zhibek/Warmup1.java
+++ src/test/java/codingbat/zhibek/Warmup1.java
@@ -1,4 +1,16 @@
 package codingbat.zhibek;
 
 public class Warmup1 {
-}
+    public static void main(String[] args) {
+                boolean weekday = true;
+                boolean vacation = false;
+                boolean result = sleepIn(weekday, vacation);
+                System.out.println(result);
+            }
+            public static boolean sleepIn(boolean weekday, boolean vacation) {
+                return !weekday || vacation;
+            }
+        }
+
+
+
