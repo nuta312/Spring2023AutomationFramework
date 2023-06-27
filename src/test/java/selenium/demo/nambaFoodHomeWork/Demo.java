b3d844bfd3098ad5813088e4711dd4f91d08fc8d
Asan Ismailov <704000320asan@gmail.com>
2023-06-27 15:54:45 +0600

    feature1

--- src/test/java/selenium/demo/nambaFoodHomeWork/Demo.java
+++ src/test/java/selenium/demo/nambaFoodHomeWork/Demo.java
@@ -1,4 +1,32 @@
 package selenium.demo.nambaFoodHomeWork;
 
-public class Demo {
+import com.digitalnomads.selenium.WebElementActions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Demo{
+        @Test
+    public void textBoxTest(){
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://nambafood.kg/");
+
+
+            WebElement img = driver.findElement(By.id("https://nambafood.kg/new_design/static/img/icons/tech_farm.png?1686712302"));
+            WebElementActions.scrollToElement(driver, img);
+
+//            WebElement foodMenuLink = driver.findElement(By.id("/food"));
+//            foodMenuLink.click();
+
+    }
 }
