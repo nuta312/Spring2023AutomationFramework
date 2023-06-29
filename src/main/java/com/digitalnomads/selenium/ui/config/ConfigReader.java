137c39ddd968eb5f01b63729ec5183ba3c5ec35e
Alisher R <alisherust@gmail.com>
2023-06-29 22:17:45 +0600

    UiBegin

---
+++ src/main/java/com/digitalnomads/selenium/ui/config/ConfigReader.java
@@ -0,0 +1,19 @@
+package com.digitalnomads.selenium.ui.config;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    private static Properties properties;
+
+    static {
+        try {
+            String path = "G:\\Spring2023AutomationFramework\\src\\main\\resources\\app.properties";
+        }catch (IOException e){
+
+        }
+
+    }
+
+}
---
+++ src/main/java/com/digitalnomads/selenium/ui/drivers/FirefoxWebDriver.java
@@ -0,0 +1,25 @@
+package com.digitalnomads.selenium.ui.drivers;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.time.Duration;
+
+public class FirefoxWebDriver {
+    public static WebDriver loadFirefoxDriver() {
+        WebDriverManager.firefoxdriver().setup();
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("--disable-extensions");
+
+        WebDriver driver = new FirefoxDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        return driver;
+    }
+
+}
