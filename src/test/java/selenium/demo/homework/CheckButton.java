72f25cea1d7995c5e81c5e2535e3a1b0d388a71e
Aigerim Moldomusaeva <moldomusaevaaigerim@gmail.com>
2023-06-29 15:21:11 +0600

    added Namba Contact Us

---
+++ src/test/java/selenium/demo/homework/CheckButton.java
@@ -0,0 +1,26 @@
+package selenium.demo.homework;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CheckButton extends BaseTestHome {
+
+//    @Test
+//    void checkButtonTest(){
+//        driver.get("https://demoqa.com/checkbox");
+//        WebElement home= driver.findElement(By.xpath());
+//        home.click();
+//        Assert.assertTrue();
+//
+//        WebElement pointer = driver.findElement(By.xpath());
+//        pointer.click();
+//
+//        WebElement desktop = driver.findElement(By.xpath());
+//        desktop.click();
+//
+//
+//
+//    }
+}
