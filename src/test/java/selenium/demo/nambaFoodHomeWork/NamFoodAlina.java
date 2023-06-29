b1ff90dd12e021a52f92d2ad78e7a43901d13398
Alina Raikanova <raikanovaalina2@gmail.com>
2023-06-29 13:31:51 +0600

    nambaFood

--- src/test/java/selenium/demo/nambaFoodHomeWork/NamFoodAlina.java
+++ src/test/java/selenium/demo/nambaFoodHomeWork/NamFoodAlina.java
@@ -12,7 +12,7 @@ import java.time.Duration;
 
 public class NamFoodAlina {
     @Test
-    public void textBoxTest() {
+    public void NambaFood() {
 
         WebDriverManager.chromedriver().setup(); // setup chrome driver
         WebDriver driver = new ChromeDriver();
@@ -20,14 +20,38 @@ public class NamFoodAlina {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15)); // waiting the elements in page
         driver.get("https://nambafood.kg/?");
 
-   WebElement submitBtn = driver.findElement(By.linkText("Еда"));
-   WebElementActions.scrollToElement(driver, submitBtn);
-   submitBtn.click();
+        WebElement food = driver.findElement(By.linkText("Еда"));
+        food.click();
 
-//  WebElement submitBtn2 = driver.findElement(By.xpath());
-//    WebElementActions.scrollToElement(driver, submitBtn2);
-//    submitBtn2.click();
-//
+       WebElement dessert = driver.findElement(By.xpath("/html/body/div[2]/main/section[3]/div/a[11]/div[1]/img"));
+       dessert .click();
+
+       WebElement cafe = driver.findElement(By.xpath("/html/body/main/section[3]/div[2]/div[3]/a[3]/div[2]/div[1]/div"));
+        cafe.click();
+
+        WebElement order = driver.findElement(By.xpath("//*[@id=\"188\"]/div/div[15]/div[2]/button"));
+       order.click();
+
+       WebElement cart=driver.findElement(By.xpath("//*[@id=\"cart\"]"));
+       cart.click();
+
+        WebElement confirm= driver.findElement(By.xpath("/html/body/main/section/div[1]/div[5]/div[11]/a/button"));
+        confirm.click();
+
+       WebElement inputName=driver.findElement(By.id("food_order_client_name"));
+       inputName.sendKeys("Alina");
+
+       WebElement inputAddress=driver.findElement(By.xpath("//*[@id=\"food_order_address\"]"));
+       inputAddress.sendKeys("Akhunbaeva 98");
+
+       WebElement inputFLat=driver.findElement(By.xpath("//*[@id=\"food_order_door_code\"]"));
+       inputFLat.sendKeys("45");
+
+        WebElement change=driver.findElement(By.xpath("//*[@id=\"food_order_money_change\"]"));
+        change.sendKeys("1000");
+
+        WebElement finish=driver.findElement(By.xpath("//*[@id=\"payment_cash\"]"));
+        finish.click();
 
     }
     }
