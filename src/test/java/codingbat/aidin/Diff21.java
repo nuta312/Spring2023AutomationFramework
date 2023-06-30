d2170cdced2484a6e8a6c1e1f5fc5abc8b7fd3c2
Iskenderov Aidin <aidin.16.08.05@gmail.com>
2023-06-30 23:52:45 +0600

    fix codingBat

--- src/test/java/codingbat/aidin/Diff21.java
+++ src/test/java/codingbat/aidin/Diff21.java
@@ -8,4 +8,29 @@ public class Diff21 {
             return (n - 21) * 2;
         }
     }
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if (aSmile == true && bSmile == true){
+            return true;
+        }else if (aSmile == false && bSmile == false){
+            return true;
+        }return false;
+    }
+    public boolean parrotTrouble(boolean talking, int hour) {
+        return (talking && (hour<7||hour>20));
+    }
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if (weekday == false || vacation == true){
+            return true;
+        }
+        return false;
+    }
+    public int sumDouble(int a, int b) {
+        if (a != b){
+            return a + b;
+        }else if (a == b){
+            int sum = a + b;
+            sum = sum + sum;
+            return sum;
+        }return 1;
+    }
 }
