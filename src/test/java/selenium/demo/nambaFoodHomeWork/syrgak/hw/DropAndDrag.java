c962f4c8e31b5807eff61619098bf8e3b0556e7f
Sergio Tollini <syrgakkenzhebaev@gmail.com>
2023-07-01 17:40:16 +0300

    HWDropAndDrag

---
+++ src/test/java/selenium/demo/nambaFoodHomeWork/syrgak/hw/DropAndDrag.java
@@ -0,0 +1,23 @@
+package selenium.demo.nambaFoodHomeWork.syrgak.hw;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import selenium.demo.lesson2.BaseTest;
+
+public class DropAndDrag extends BaseTest {
+    @Test
+    void dropAndDrag() throws InterruptedException {
+        Actions actions = new Actions(driver);
+        driver.get("https://demoqa.com/droppable");
+        WebElement dropMe = driver.findElement(By.id("draggable"));
+        WebElement dropBox = driver.findElement(By.xpath("//div[@id='simpleDropContainer']//div[@id='droppable']"));
+        actions.moveToElement(dropMe).clickAndHold(dropMe).moveToElement(dropBox).release().perform();
+        Thread.sleep(3000);
+        WebElement result = driver.findElement(By.xpath("//p[normalize-space()='Dropped!']"));
+        Assert.assertEquals(result.getText(),"Dropped!");
+
+    }
+}
