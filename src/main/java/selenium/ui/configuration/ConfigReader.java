af88428715e055a30b3e0771b194205e40f1374c
Aigerim Moldomusaeva <moldomusaevaaigerim@gmail.com>
2023-07-01 12:59:12 +0600

    Config added

--- src/main/java/selenium/ui/configuration/ConfigReader.java
+++ src/main/java/selenium/ui/configuration/ConfigReader.java
@@ -1,9 +1,21 @@
 package selenium.ui.configuration;
 
+import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigReader {
     private static Properties properties;
 
+//    static{
+//        try{
+//            String path ="src/main/resources/app.properties";
+//        } catch (IOException e){
+//            return null
+//        }
 
-}
+
+
+
+    }
+
+
