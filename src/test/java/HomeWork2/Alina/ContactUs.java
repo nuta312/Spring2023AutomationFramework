9c3c26d52d8dddc4449f7f5c0433e266dd019d65
Alina Raikanova <raikanovaalina2@gmail.com>
2023-07-03 05:08:18 +0600

    homework2

---
+++ src/test/java/HomeWork2/Alina/ContactUs.java
@@ -0,0 +1,15 @@
+package HomeWork2.Alina;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import selenium.demo.lesson2.BaseTest;
+
+public class ContactUs extends BaseTest {
+    @Test
+    void contactUs(){
+        driver.get("https://nambafood.kg/");
+        String contact=driver.findElement(By.xpath("/html/body/main/section[8]/div/h3[2]")).getText();
+        System.out.println(contact);
+    }
+}
+
