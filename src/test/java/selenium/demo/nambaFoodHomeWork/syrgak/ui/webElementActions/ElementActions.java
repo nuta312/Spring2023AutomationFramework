0b56fc1d07f2548bff6d6072f9881c97bf0556ba
Sergio Tollini <syrgakkenzhebaev@gmail.com>
2023-07-04 19:20:42 +0300

    Pattern

--- src/test/java/selenium/demo/nambaFoodHomeWork/syrgak/ui/webElementActions/ElementActions.java
+++ src/test/java/selenium/demo/nambaFoodHomeWork/syrgak/ui/webElementActions/ElementActions.java
@@ -1,35 +1,29 @@
 package selenium.demo.nambaFoodHomeWork.syrgak.ui.webElementActions;
 
-import org.openqa.selenium.WebDriver;
+import com.digitalnomads.selenium.ui.driverFactory.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import selenium.demo.nambaFoodHomeWork.syrgak.ui.driverFactory.Driver;
-
 import java.time.Duration;
-
 public class ElementActions {
-    public ElementActions waitButtonToBeClickable (WebElement element){
-        new WebElement(Driver.getDriver(), Duration.ofSeconds(15))
+    public ElementActions waitButtonToBeClickable(WebElement element) {
+        new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(15))
                 .until(ExpectedConditions.elementToBeClickable(element));
         return this;
     }
-    public ElementActions waitButtonToBeDisplayed (WebElement element) throws IllegalAccessException {
+    public ElementActions waitElementToBeDisplayed(WebElement element) {
         new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(15))
                 .until(ExpectedConditions.visibilityOf(element));
         return this;
     }
-    public ElementActions clickTheButton (WebElement element){
+    public ElementActions clickTheButton(WebElement element) {
         waitButtonToBeClickable(element);
         element.click();
         return this;
     }
-    public ElementActions writeText(WebElement element, String txt){
+    public ElementActions writeText(WebElement element,String txt){
         waitElementToBeDisplayed(element);
         element.sendKeys(txt);
         return this;
-    }
-
-    private void waitElementToBeDisplayed(WebElement element) {
     }
 }
