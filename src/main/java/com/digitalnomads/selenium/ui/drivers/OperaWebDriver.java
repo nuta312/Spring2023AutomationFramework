2d52b798d79e4bc9b13ce82b147abc8ed5a87e09
Janyl <janyla_i@mail.ru>
2023-07-05 19:18:13 +0600

    NewClass

--- src/main/java/com/digitalnomads/selenium/ui/drivers/OperaWebDriver.java
+++ src/main/java/com/digitalnomads/selenium/ui/drivers/OperaWebDriver.java
@@ -1,24 +1,22 @@
 package com.digitalnomads.selenium.ui.drivers;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import io.github.bonigarcia.wdm.webdriver.WebDriverBrowser;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 
 import java.time.Duration;
 
-public class EdgeWebDriver {
+public class OperaWebDriver {
 
-    public static WebDriver loadEdgeWebDriver(){
+    public static WebDriver loadOperaWebDriver(){
         WebDriverManager.edgedriver().setup();
-        EdgeOptions options = new EdgeOptions();
-        options.addArguments("--disable-extensions");
-
+        EdgeDriver options = new EdgeDriver();
         WebDriver driver = new EdgeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         return driver;
     }
+
 
 }
